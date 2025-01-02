be373d363d309a39a87253fa6ec6018fa5523c4e
Karthiknaidu9 <karthiknaidukonchada35@gmail.com>
2025-01-02 16:54:13 +0530

    add status change of todos

--- frontend/src/components/TodosList.jsx
+++ frontend/src/components/TodosList.jsx
@@ -7,45 +7,79 @@ const TodosList = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
   const [data, setData] = useState([]);
   const [range, setRange] = useState(0);
+  const [lengthofpages, setlength] = useState([]);
   const ITEMS_PER_PAGE = 5;
 
+  async function fetchData() {
+    try {
+      const res = await axios.get("http://localhost:5000/tasks", {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
+        },
+      });
+      setIsAuthenticated(true);
+      setData(res.data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+      setIsAuthenticated(false);
+    }
+  }
   useEffect(() => {
     if (!sessionStorage.getItem("authToken")) {
       setIsAuthenticated(false);
       return;
     }
-
-    // Fetch the data if token exists
-    async function fetchData() {
-      try {
-        const res = await axios.get("http://localhost:5000/tasks", {
-          headers: {
-            Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
-          },
-        });
-        setIsAuthenticated(true);
-        setData(res.data);
-      } catch (error) {
-        console.error("Error fetching tasks:", error);
-        setIsAuthenticated(false);
-      }
-    }
     fetchData();
   }, []);
+
+  useEffect(() => {
+    let len = Math.floor(data.length / 5);
+    if (len % 5 !== 0) {
+      len++;
+    }
+    const numbers = Array.from({ length: len }, (_, index) => index + 1);
+    setlength(numbers);
+    // console.log(numbers);
+  }, [data]);
 
   if (isAuthenticated === false) {
     return <Navigate to={"/login"} />;
   }
 
-  if (isAuthenticated === null) {
+  if (isAuthenticated === null || data.length === 0) {
     return <div>Loading...</div>;
   }
 
   // Get the current page data
+  function handlePagechange(ind) {
+    console.log(ind);
+    setRange(ind * 5);
+  }
+  async function handleStatuschange(e, item) {
+    console.log(e.target.value);
+    console.log(item);
+    try {
+      await axios.put(
+        `http://localhost:5000/tasks/${item._id}`,
+        {
+          task: item.task,
+          status: e.target.value,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
+          },
+        }
+      );
+      fetchData();
+    } catch (e) {
+      console.log(e);
+    }
+  }
   const paginatedData = data.slice(range, range + ITEMS_PER_PAGE);
-
+  console.log(range);
   return (
-    <div className="header">
+    <div className="container">
       <h1>List of All TODOS Created</h1>
       <div className="todos-list">
         {paginatedData.map((item, index) => (
@@ -55,14 +89,17 @@ const TodosList = () => {
               <select
                 id={`options-${index}`} // Unique ID for each dropdown
                 value={item.status}
-                onChange={(event) => {}}
+                onChange={(e) => {
+                  handleStatuschange(e, item);
+                }}
                 style={{ marginLeft: "10px", padding: "5px" }}
+                className={`${item.status}`}
               >
                 <option value={item.status}>{item.status}</option>
                 {["todo", "doing", "done"]
                   .filter((status) => status !== item.status)
                   .map((status, i) => (
-                    <option key={i} value={status}>
+                    <option key={i} value={status} className={`${status}`}>
                       {status}
                     </option>
                   ))}
@@ -82,6 +119,24 @@ const TodosList = () => {
         >
           &lt;
         </button>
+        {lengthofpages &&
+          lengthofpages.length > 0 &&
+          lengthofpages.map((item, index) => {
+            return (
+              <div
+                className={`${
+                  Math.floor(range / 5) + 1 === index + 1
+                    ? "active"
+                    : "unactive"
+                }`}
+                onClick={() => handlePagechange(index)}
+                key={index}
+              >
+                {index + 1}
+              </div>
+            );
+          })}
+
         <button
           onClick={() =>
             setRange((prev) =>

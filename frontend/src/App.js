dcc660a00da8693469ef0c44a005558cf523eb6c
Ganesh-Kasibisi <146353522+Ganeshkasibisi@users.noreply.github.com>
2025-01-02 17:22:13 +0530

    added signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./components/SignupPage.js";
+import SearchBar from "./components/SearchBar.js";
 
 function App() {
-  return <div className="App">working</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Signup />} /> {/* Signup page */}
+          <Route path="/search" element={<SearchBar />} /> {/* Search page */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
